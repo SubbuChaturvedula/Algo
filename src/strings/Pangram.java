13086f686888f70af4ac0cd983fbcf762cf0bc79
subbucn.it@gmail.com <$Lnmurthy18>
2016-01-27 10:04:06 -0800

    Pangram

---
+++ src/strings/Pangram.java
@@ -0,0 +1,33 @@
+package strings;
+
+public class Pangram {
+	// pangrams are sentences constructed by using every letter of the alphabet
+	// at least once.
+
+	public static void main(String[] args) {
+		// Scanner in = new Scanner(System.in);
+		// String inputString = in.nextLine();
+		String inputString = "We promptly judged antique ivory buckles for the next prize";
+		System.out.println(isPangram(inputString));
+		// in.close();
+	}
+
+	public static String isPangram(String input) {
+		boolean[] charBM = new boolean[26];
+		String inputLower = input.toLowerCase();
+		char aCharNum = 'a';
+		for (int i = 0; i < inputLower.length(); i++) {
+			if (inputLower.charAt(i) != ' ') {
+				char c = inputLower.charAt(i);
+				charBM[c % aCharNum] = true;
+			}
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (charBM[i] == false) {
+				return "not pangram";
+			}
+		}
+		return "pangram ";
+	}
+}
