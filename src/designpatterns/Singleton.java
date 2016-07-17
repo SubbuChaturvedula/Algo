bd8ae2c0417f1797797b6f3b369e70cb00e6f690
subbucn.it@gmail.com <$Lnmurthy18>
2016-07-16 20:15:03 -0700

    Double lock Singleton

---
+++ src/designpatterns/Singleton.java
@@ -0,0 +1,24 @@
+package designpatterns;
+
+
+public class Singleton {
+
+	private static volatile Singleton _instance;
+
+	private Singleton() {
+	}
+	/**
+	 * * Double checked locking code on Singleton * @return Singleton instance
+	 */
+	public static Singleton getInstance() {
+		if (_instance == null) {
+			synchronized (Singleton.class) {
+				if (_instance == null) {
+					_instance = new Singleton();
+				}
+			}
+		}
+		return _instance;
+	}
+}
+
